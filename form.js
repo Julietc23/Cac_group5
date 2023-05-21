1dc1bd3d88b3882e97721b9411fb48fbf3d0e0d5
Agustin <agustinetchegoyen7@gmail.com>
2023-05-20 22:37:46 -0300

    finalizado el html y el css del formulario

--- form.js
+++ form.js
@@ -1,26 +1,53 @@
 //validacion del formulario
-function validarFormulario(){
-    //obtengo los valores de cada campo que el usuario lleno
-    let nombre = document.getElementById("nombre").value.trim();
-    let mail = doocument.getElementById("mail").value.trim();
-    let tel = document.getElementById("tel").value.trim();
-    let consulta = document.getElementById("consulta").value.trim();
-    //el if me sirve para que si alguno de los campos esta vacio no se pueda mandar
-    if(nombre === "" || mail === "" || tel === "" || consulta ===""){
-        alert("Por favor, complete todos los campos para enviar la consulta.")
+function validarFormulario() {
+    let nombre = document.getElementById("nombre").value.trim()
+    let mail = document.getElementById("mail").value.trim()
+    let tel = document.getElementById("tel").value.trim()
+    let consulta = document.getElementById("consulta").value.trim()
+  
+    if (nombre === "" || mail === "" || tel === "" || consulta === "") {
+      alert("Por favor, complete todos los campos para enviar la consulta.")
+      return false
+    }
+  
+    for (var i = 0; i < nombre.length; i++) {
+      let valorLetra = nombre.charCodeAt(i)
+      if (
+        !(valorLetra >= 65 && valorLetra <= 90) &&
+        !(valorLetra >= 97 && valorLetra <= 122) &&
+        !(valorLetra === 32)
+      ) {
+        alert("El nombre puede contener solamente caracteres alfabéticos.")
+        return false
+      }
+    }
+  
+    if (!mail.includes("@")) {
+      alert("Ingrese una dirección de correo electrónico válida.")
+      return false
+    }
+
+    if(!(tel.length<=10)){
+        alert("Recuerde que el número de teléfono no puede tener mas de 10 dígitos.")
         return false
     }
 
-    for(var i = 0; i < nombre.length; i++){
-        let valorLetra = nombre.charCodeAt(i)
-        if(!(valorLetra >= 65 && valorLetra <= 95) ||(valorLetra >= 97 && valorLetra <= 122) || (valorLetra=== 32)){
-            alert("El nombre puede contener solamente caracteres alfabeticos")
+    for(var k=0;k < tel.length;k++){
+        let numeroTel = tel.charAt(k)
+        if(numeroTel<"0" || numeroTel>"9"){
+            alert("El número de teléfono solo puede contener caracteres numericos.")
             return false
         }
     }
-    
 
-}
+    if(consulta.length>700){
+        alert("El campo de consulta tiene mas de los caracteres totales admitidos. Porfavor resuma su consulta.")
+        return false
+    }
+  
+    alert("Consulta enviada correctamente!")
+    return true
+  }
 
 /*Funciones css para el formulario*/
 const input = document.querySelectorAll(".input") /*Creo una arreglo que obtenga todo de la clase input*/
