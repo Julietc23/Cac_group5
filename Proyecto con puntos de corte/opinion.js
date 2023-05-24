c7a3ba54fc07ff721bca23fe865c5e2316a73aee
Agustin <agustinetchegoyen7@gmail.com>
2023-05-24 02:34:23 -0300

    pagina terminada (posiblemente falten retoques)

--- Proyecto con puntos de corte/opinion.js
+++ Proyecto con puntos de corte/opinion.js
@@ -4,7 +4,8 @@ const miApp = createApp({
         return {
             opinionActuales: "",
             opinionesHechas: 0,
-            opinionUsuario: ""
+            opinionUsuario: "",
+            op1: "Me encantaron los cursos!!"
         }
     },
 
@@ -15,45 +16,43 @@ const miApp = createApp({
                 .then(datos => {
                     this.opinionActuales +=
                         `<div class="seccionOp">
-                    <img src="${datos.results[0].picture.large}"></img>
-                    <button class="botonopiniones" onclick="opinionUser1(${this.opinionesHechas})">ver opinion<button>
+                        <h4>usuario:</h4>
+                        <h3>${datos.results[0].login.username}</h3><br>
+                    <img src="${datos.results[0].picture.large}">
+                    <p class="titulocent">${this.op1}</p>
                     <h1>${datos.results[0].name.first}</h1><br>
-                    <h3>${datos.results[0].login.username}</h3><br>
-                    <h4>Usuario</h4></div>`
+                    </div>`
                     this.opinionesHechas++
                 })
         },
-        colgarOpinion(){
+        colgarOpinion() {
             let opinion = document.getElementById("input").value
-            let nombre  = document.getElementById("nombre").value
+            let nombre = document.getElementById("nombre").value
             let usuario = document.getElementById("usuario").value
-            if(opinion.length >150){
+            if (opinion.length > 150) {
                 alert("Superaste el limite de caracteres permitidos. Haz una opinion mas corta.")
                 return false
-            } else{
+            } else {
                 this.opinionUsuario +=
-                `<div class="seccionOp">
-                <button class="botonopiniones" onclick="opinionUser0()">Ver Opinion<button>
+                    `<div class="seccionOp secus">
+                <h4>usuario:</h4>
+                <h3>${usuario}</h3>
+                <img class="imgusuario" src="imagenes/usuario.png">
+                <div class="textoOp">
+                <p class="titulocent">${opinion}</p>
+                </div>
+                <div>
                 <h1>${nombre}</h1><br>
-                <h3>${usuario}</h3><br>
-                <h4>usuario</h4></div>`
+                </div>
+                </div>`
                 this.opinionesHechas++
             }
-        }                
+        },
     }
 
 })
 miApp.mount('#opinion-app')
 
-function opinionUser0(){
-    let opinion = document.getElementById("input").value
-    console.log(opinion)
-}
-function opinionUsser1(valor){
-    if(valor % 2 === 0){
-        return console.log("opinion par")
-    } else{
-        return console.log("opinion impar")
-    }
-}
+
+
 
